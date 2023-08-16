a95d44dfd3d53eb243e720465948f173ec6619c6
Anupam <92312729+amaze18@users.noreply.github.com>
2023-08-16 22:35:54 +0530

    footer update

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ from htbuilder.units import percent, px
 from htbuilder.funcs import rgba, rgb
 
 
+
 def image(src_as_string, **style):
     return img(src=src_as_string, style=styles(**style))
 
@@ -30,7 +31,47 @@ def layout(*args):
      .stApp { bottom: 105px; }
     </style>
     """
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="black",
+        text_align="center",
+        height="auto",
+        opacity=1
+    )
 
+    style_hr = styles(
+        display="block",
+        margin=px(8, 8, "auto", "auto"),
+        border_style="inset",
+        border_width=px(2)
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+ 
 from PIL import Image
 
 
@@ -119,4 +160,18 @@ myargs = [
     link("https://i-venture.org/", "ISB"),
       #   image('https://i.imgur.com/thJhzOO.png')),
 ]
-layout(myargs)
+
+def footer():
+    myargs = [
+        "Made in ",
+        image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
+              width=px(25), height=px(25)),
+        " with ❤️ by ",
+        link("https://twitter.com/ChristianKlose3", "@ChristianKlose3"),
+        br(),
+        link("https://buymeacoffee.com/chrischross", image('https://i.imgur.com/thJhzOO.png')),
+    ]
+    layout(*myargs)
+
+
+footer()
